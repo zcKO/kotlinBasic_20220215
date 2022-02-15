ec00097886616b074fcc9906bd3103ef732b5d2c
JaeCheon <studyJcHub@gmail.com>
2022-02-15 10:07:13 +0900

    버튼이 눌리면, 로그 찍어보기

--- app/src/main/java/com/jc/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/jc/kotlinbasic_20220215/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.jc.kotlinbasic_20220215
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        btnLog.setOnClickListener {
+            Log.d("메인화면", "로그 버튼 눌림")
+        }
 
     }
 
