fbd6002680e2dd92522ea35ba455585d5502bb9b
JaeCheon <studyJcHub@gmail.com>
2022-02-15 10:15:43 +0900

    비교용 버튼 추가, 주석 작성

--- app/src/main/java/com/jc/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/jc/kotlinbasic_20220215/MainActivity.kt
@@ -11,9 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        btn log가 눌리면 어떤 코드를 실행할지, 할 일을 설정.
         btnLog.setOnClickListener {
+//            로그를 찍어보는 기능 실행. => btnLog 가 클릭 될 때 마다 실행.
             Log.d("메인화면", "로그 버튼 눌림")
         }
+
 
     }
 
